45e4cbead47cc407e5a441e427f37c57287e0a23
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-08-28 17:35:16 +0200

    New changes without better progress

--- 2-spring-framework/src/main/java/info/jab/ms/jsp/Main.java
+++ 2-spring-framework/src/main/java/info/jab/ms/jsp/Main.java
@@ -1,16 +1,21 @@
 package info.jab.ms.jsp;
 
+import jakarta.annotation.PostConstruct;
 import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
-
-import org.springframework.web.context.support.StaticWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.RouterFunctions;
-import org.springframework.web.servlet.function.ServerResponse;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.io.File;
 
 public class Main {
 
@@ -18,33 +23,62 @@ public class Main {
 		startTomcat();
 	}
 
-	static StaticWebApplicationContext applicationContext(RouterFunction<ServerResponse> routes) {
-		StaticWebApplicationContext applicationContext = new StaticWebApplicationContext();
-		// Add JSP View Resolver
-		applicationContext.registerBean(ViewResolver.class, () -> {
-			InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-			viewResolver.setViewClass(JstlView.class);
-			viewResolver.setPrefix("WEB-INF/hello.jsp"); // Path to JSP files
-			viewResolver.setSuffix(".jsp");
-			return viewResolver;
-		});
-		return applicationContext;
-	}
-
 	static void startTomcat() throws Exception {
 		Tomcat tomcat = new Tomcat();
 		tomcat.getConnector().setPort(8080);
+
 		Context context = tomcat.addContext("", System.getProperty("java.io.tmpdir"));
-		DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext(routes()));
-		Tomcat.addServlet(context, "dispatcherServlet", dispatcherServlet).addMapping("/");
-		tomcat.start();
-		tomcat.getServer().await();
+
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(SpringConfig.class);
+		appContext.refresh();
+
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(appContext);
+		Wrapper wrapper = context.createWrapper();
+		wrapper.setName("dispatcherServlet");
+		wrapper.setServlet(dispatcherServlet);
+		context.addChild(wrapper);
+		wrapper.setLoadOnStartup(1);
+		wrapper.addMapping("/");
+
+		try {
+			tomcat.start();
+			tomcat.getServer().await();
+		} catch (LifecycleException e) {
+			System.out.println("Katakroker");
+		}
 	}
 
-	static RouterFunction<ServerResponse> routes() {
-		return RouterFunctions.route()
-				.GET("/hello", request -> ServerResponse.ok().render("hello")) // Handle JSP rendering
-				.build();
+	@Configuration
+	static class SpringConfig {
+
+		@Controller
+		public static class MyRestController {
+
+			@GetMapping("/hello2")
+			public String hello() {
+				return "hello2";
+			}
+
+			@PostConstruct
+			private void postConstruct() {
+				System.out.println("Running RestController");
+			}
+		}
+
+		@Bean
+		public ViewResolver viewResolver() {
+			InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+			viewResolver.setViewClass(JstlView.class);
+			viewResolver.setPrefix("/WEB-INF/");
+			viewResolver.setSuffix(".jsp");
+			return viewResolver;
+		}
+
+		@PostConstruct
+		private void postConstruct() {
+			System.out.println("Running Spring Config");
+		}
 	}
 
 }
